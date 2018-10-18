b95f81ffc468950172821759392f08ea82c4af68
Thomas Johnson <tjohnson1106@gmail.com>
2018-10-18 14:24:29 -0400

    initialized app state in app component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import classnames from "classnames";
+import snakeCase from "lodash/snakeCase";
+
+import Switch from "./components/ToggleSwitch";
+import "./App.css";
+
+const ACTIVITIES = [
+  "News Feeds",
+  "Likes and Comments",
+  "Live Stream",
+  "Upcoming Events",
+  "Friend Requests",
+  "Nearby Friends",
+  "Birthdays",
+  "Account Sign-In"
+];
 
 class App extends Component {
+  state = {
+    enabled: false,
+    only: ACTIVITIES.map(snakeCase)
+  };
+
+  toggleNotifications = ({ enabled }) => {
+    const { only } = this.state;
+    this.setState({
+      enabled,
+      only: enabled ? only : ACTIVITIES.map(snakeCase)
+    });
+  };
+
   render() {
+    const { enabled } = this.state;
+
+    const headingClasses = classnames(
+      "font-weight-light h2 mb-0 pl-4",
+      enabled ? "text-dark" : "text-secondary"
+    );
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App position-absolute text-left d-flex justify-content-center align-items-start pt-5 h-100 w-100">
+        <div className="d-flex flex-wrap mt-5" style={{ width: 600 }}>
+          <div className="d-flex p-4 border rounded align-items-center w-100">
+            <Switch
+              theme="default"
+              className="d-flex"
+              enabled={enabled}
+              onStateChanged={this.toggleNotifications}
+            />
+
+            <span className={headingClasses}>Notifications</span>
+          </div>
+
+          {/_ ...Notification options here... _/}
+        </div>
       </div>
     );
   }
--- src/components/ToggleSwitch/index.js
+++ src/components/ToggleSwitch/index.js
@@ -3,8 +3,8 @@ import PropTypes from "prop-types";
 import isString from "lodash/isString";
 import isFunction from "lodash/isFunction";
 import isBoolean from "lodash/isBoolean";
-
 import classnames from "classnames";
+
 import "./index.css";
 
 class ToggleSwitch extends Component {
